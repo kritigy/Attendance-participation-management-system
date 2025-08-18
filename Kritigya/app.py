0981606bd14efbfe5bdcf6b813e1e9da9a6e81e8
kritigya katwal <126181135+kritigy@users.noreply.github.com>
2025-08-18 19:39:25 +0545

    Delete Kritigya/app.py

--- Kritigya/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from routes.tasks import tasks_bp
-
-app = Flask(__name__)
-
-app.register_blueprint(tasks_bp, url_prefix='/') 
-
-if __name__ == '__main__':
-    app.run(debug=True)
